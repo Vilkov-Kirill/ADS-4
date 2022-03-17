303274d4b2a22055ef1a0fbfdffaa1bd7104ba09
Vilkov-Kirill <99217802+Vilkov-Kirill@users.noreply.github.com>
2022-03-18 02:28:23 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -64,7 +64,7 @@ TEST(lab2, test3) {
     t1 = end1 - begin1;
     t2 = end2 - begin2;
     t3 = end3 - begin3;
-    printf("\n%i %i %i\n", t1, t2, t3);
+    printf("\n%li %li %li\n", t1, t2, t3);
     EXPECT_TRUE((t1 > t2) && (t1 > t3) && (t2 > t3));
     delete[] arr;
 }
